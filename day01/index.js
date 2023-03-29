8a76fdecb283ec975d0b6d13add990b8b8b06912
unknown <kanaramsaini980@gmail.com>
2023-03-29 14:18:19 +0530

    day01

---
+++ day01/index.js
@@ -0,0 +1,10 @@
+const fs=require('fs');
+fs.writeFileSync('red.txt',"Hello sir");
+
+let a=20;
+let b=30;
+let c=20*30;
+console.log(`Multiple:${a}*${b}=${c}`)
+
+const fss=require('fs');
+fss.writeFileSync('myfile.txt',"Hello Sir My Name is Kanaram Saini");
